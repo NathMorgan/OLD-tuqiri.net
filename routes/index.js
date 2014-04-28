8f2752011b9d67981f650bab48cf8620169ae2cd
Tuqiri <tuqiri@tuqiri.net>
2014-04-28 19:36:10 +0100

    Added actions for different pages

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,27 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Home' });
 };
+
+/*
+ * GET about.
+ */
+
+exports.about = function(req, res){
+    res.render('about', { title: 'About' });
+};
+
+/*
+ * GET projects.
+ */
+
+exports.projects = function(req, res){
+    res.render('projects', { title: 'Projects' });
+};
+
+/*
+ * GET contact.
+ */
+
+exports.contact = function(req, res){
+    res.render('contact', { title: 'Contact' });
+};
