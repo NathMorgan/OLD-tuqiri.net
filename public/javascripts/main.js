d9f15eb9e9ba23ded0399639855590aa1e1fc762
Tuqiri <tuqiri@tuqiri.net>
2014-04-29 10:25:59 +0100

    Added jQuery to control the page change

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,24 @@
 $(document).ready(function(){
-    var mainCanvas = document.getElementById('loading-canvas');
-    var p1 = 0;
+    var currentPage = $('#home');
+    var prevPage = $('#home');
 
-    var circle = new ProgressCircle({
-        canvas: mainCanvas,
-        minRadius: 1,
-        arcWidth: 400
-    });
+    $(".nav-link").click(function(){
+        var page = $(this)
 
+        prevPage = currentPage
+        prevPage.css({"color" : "#4d4d4d"});
+        prevPage.css({"cursor" : "pointer"});
 
-    circle.addEntry({
-        fillColor: 'rgba(0, 50, 255, 1)',
-        progressListener: function() {return p1;}
-    }).start(1);
+        currentPage = page;
+        currentPage.css({"color" : "darkblue"});
+        currentPage.css({"cursor" : "auto"});
 
-    var checkinterval = setInterval(function() {
-        $("#loading-container p").html(Math.round(p1 * 100) + "%");
-        if(p1 > 1){
-            clearInterval(checkinterval);
-            $('#canvas-container').hide('slide', {direction: 'right'}, 500, function(){
+        history.pushState({id: page.attr("id")}, '', page.attr("id"));
 
-            });
-        }
-        else{
-            p1 += 0.0025
-        }
-    }, 1);
+        console.log(page.attr("id"));
+
+        console.log(prevPage.attr("id"));
+    })
 });
 
 
