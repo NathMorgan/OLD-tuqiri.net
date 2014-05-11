98302d5188f3bed3a828669140f995ff7aac57d3
Tuqiri <tuqiri@tuqiri.net>
2014-05-11 12:39:57 +0100

    Added get projects to the portfolio route

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,7 +1,15 @@
 /*
- * GET portfolio.
+ * GET index.
  */
 
 exports.index = function(req, res){
     res.render('portfolio', { title: 'Portfolio' });
+};
+
+/*
+ * GET projects.
+ */
+
+exports.getProjects = function(req, res){
+    res.render('');
 };
